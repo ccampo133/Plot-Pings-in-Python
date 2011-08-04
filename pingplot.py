7b0e753f6a5327e7055fef8d6e0040f86c4ac3ed
Christopher Campo <ccampo@gmail.com>
2011-08-04 02:39:02 -0400

    Updated readme and fixed weird indentations in the code

--- pingplot.py
+++ pingplot.py
@@ -48,34 +48,35 @@ def write_log(logfile, outstr):
 	
 # produces ping vs time plot
 def plot_gen(ping, now, t, nans, host):
-		datestr   = now[0].ctime().split()
-		datestr   = datestr[0] + " " + datestr[1] + " " + datestr[2] + " " + datestr[-1]
-		startdate = time.ctime(t[0])
-		plt.figure(figsize=(25,8))
-		plt.plot(now[~nans], ping[~nans], drawstyle='steps')
-		plt.title("Ping Results for {0}".format(host))
-		plt.ylabel("Latency [ms]")
-		plt.xlabel("Time, {0} [GMT -{1} hrs]".format(datestr, time.timezone/3600))
-		plt.xticks(size=10)
-		plt.yticks(size=10)
-		plt.ylim(ping[~nans].min()-5, ping[~nans].max()+5)
+	datestr   = now[0].ctime().split()
+	datestr   = datestr[0] + " " + datestr[1] + " " + datestr[2] + " " + datestr[-1]
+	startdate = time.ctime(t[0])
+	plt.figure(figsize=(25,8))
+	plt.plot(now[~nans], ping[~nans], drawstyle='steps')
+	plt.title("Ping Results for {0}".format(host))
+	plt.ylabel("Latency [ms]")
+	plt.xlabel("Time, {0} [GMT -{1} hrs]".format(datestr, time.timezone/3600))
+	plt.xticks(size=10)
+	plt.yticks(size=10)
+	plt.ylim(ping[~nans].min()-5, ping[~nans].max()+5)
+	
+	# plot packet losses
+	start  = []
+	finish = []
+	for i in range(len(nans)):
+		if nans[i] == True:
+			if i == 0:
+				start.append(i)
+			elif nans[i] != nans[i-1]:
+				start.append(i)
+			if i != len(nans) and nans[i+1] != nans[i]:
+				finish.append(i)
+				
+	# add the red bars for bad pings
+	for i in range(len(start)):
+		plt.axvspan(now[start[i]], now[finish[i]+1], color='red')
+	return
 		
-		# plot packet losses
-		start  = []
-		finish = []
-		for i in range(len(nans)):
-			if nans[i] == True:
-				if i == 0:
-					start.append(i)
-				elif nans[i] != nans[i-1]:
-					start.append(i)
-				if i != len(nans) and nans[i+1] != nans[i]:
-					finish.append(i)
-					
-		# add the red bars for bad pings
-		for i in range(len(start)):
-			plt.axvspan(now[start[i]], now[finish[i]+1], color='red')
-
 # main
 def main(argv=None):
 	# for interactive mode
@@ -126,13 +127,15 @@ def main(argv=None):
 	print("{0:>17} {1}".format("Ping interval:", str(dt) + " s"))
 	print("{0:>17} {1}".format("Packets per ping:", n))
 	print("\n\nPress ESC to quit...\n")
-	print("{0:^15} {1:^15} {2:^15} {3:^15} {4:^15}\n".format(
-															"AVG. PING", 
-															"PACKET LOSS", 
-															"NUM. PINGS", 
-															"NUM. TIMEOUTS", 
-															"TIME ELAPSED"
-															))
+	print("{0:^15} {1:^15} {2:^15} {3:^15} {4:^15}\n"
+			.format(
+			"AVG. PING", 
+			"PACKET LOSS", 
+			"NUM. PINGS", 
+			"NUM. TIMEOUTS", 
+			"TIME ELAPSED"
+			))
+			
 	while True:
 		# quit on escape
 		if msvcrt.kbhit() and ord(msvcrt.getch()) == 27: break	
@@ -161,12 +164,12 @@ def main(argv=None):
 		deltat = datetime.timedelta(seconds=(round(time.time() - t[0], 0)))
 		sys.stdout.write("\r{0:^15.8} {1:^15.10} {2:^15} {3:^15} {4:^15}"
 						.format(
-								str(round(mping, 2))+" ms", 
-								str(round(mloss, 2))+" %",
-								cnt*n, 
-								len(ping[nans]), 
-								str(deltat))
-								)
+						str(round(mping, 2))+" ms", 
+						str(round(mloss, 2))+" %",
+						cnt*n, 
+						len(ping[nans]), 
+						str(deltat)
+						))
 	
 	print("\n")
 	# close log file
